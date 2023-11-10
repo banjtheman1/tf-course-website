37e622f5094a41735e25dfdeff762753e4399cb6
baolaniyan1 <baolaniyan@deloitte.com>
2023-11-10 18:24:12 +0000

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.41.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket  = "roidtc-projects-217-tf-state"
+    prefix  = "terraform/state"
+  }
+
+
+
+}
+
+
+provider "google" {
+
+  project = var.project_id
+
+}
